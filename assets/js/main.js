c9702facee66882f0248a3c700d259167f052ee8
Meet Parmar <meetparmar071@gmail.com>
2021-04-08 11:41:26 +0530

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -189,37 +189,4 @@
 
 
 
-        // function sendEmail() {
-        //     var name = $("#name");
-        //     var email = $("#email");
-        //     var subject = $("#message");
-        //     var body = $("#body");
-
-        //     if (isNotEmpty(name) && isNotEmpty(email) && isNotEmpty(subject) && isNotEmpty(body)) {
-        //         $.ajax({
-        //            url: 'sendEmail.php',
-        //            method: 'POST',
-        //            dataType: 'json',
-        //            data: {
-        //                name: name.val(),
-        //                email: email.val(),
-        //                subject: subject.val(),
-        //                body: body.val()
-        //            }, success: function (response) {
-        //                 $('#myForm')[0].reset();
-        //                 $('.sent-notification').text("Message Sent Successfully.");
-        //            }
-        //         });
-        //     }
-        // }
-
-        // function isNotEmpty(caller) {
-        //     if (caller.val() == "") {
-        //         caller.css('border', '1px solid red');
-        //         return false;
-        //     } else
-        //         caller.css('border', '');
-
-        //     return true;
-        // }
-  
+       
--- script.js
+++ script.js
@@ -5,7 +5,7 @@ btn.addEventListener('click', () => {
         Host : "smtp.mailtrap.io",
         Username : "a983df0fec85f8",
         Password : "bbc99f4421846e",
-        To : 'meetparmar071@gmail.com',
+        To : 'xyz@gmail.com',
         From : document.getElementById('email').value,
         Subject : "Contact us by visiter",
         Body : document.getElementById('email').value + "<br>" + document.getElementById('name').value +"<br>" + document.getElementById('subject').value + document.getElementById('email').value + "<br>"
